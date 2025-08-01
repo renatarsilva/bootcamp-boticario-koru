9f97655d187ecf49d65d8687e2b82d2203e3ee18
Renata <renata.rodriguesdasilva@outlook.com>
2025-08-01 00:38:36 -0300

    feat: update parImpar

--- 2025-07-09-variaveis-loops/condicional/if.js
+++ 2025-07-09-variaveis-loops/condicional/if.js
@@ -1,9 +1,10 @@
-let se = function (a) {
-  if (a >= 18) {
+let nota = function (a, b, c, d) {
+  let resultado = (a + b + c + d) / 4;
+  if (resultado > 7) {
     return true;
   } else {
     return false;
   }
 };
 
-console.log(se(18));
+console.log(nota(7, 9, 5, 10));
---
+++ 2025-07-09-variaveis-loops/condicional/parImpar.js
@@ -0,0 +1,9 @@
+let parImpar = function (a, b) {
+  if (a % b == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(parImpar(10, 2));
